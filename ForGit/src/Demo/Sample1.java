641791062e52dc4e107af49820c47f3db3c232c2
mnetke.dev <mnetke.dev@DESKTOP-0BP7HQF>
2022-12-05 07:59:55 +0530

    1st class updated

--- ForGit/src/Demo/Sample1.java
+++ ForGit/src/Demo/Sample1.java
@@ -4,7 +4,9 @@ public class Sample1
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("1st Repository");	
+	System.out.println("1st Repository");
+	System.out.println("2nd Repository");
+
 	}
 
 }
